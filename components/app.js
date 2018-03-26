4782521fa3d460928b0f54ba4213ae8482fbe948
Kacper Stroisz <kacperstroisz@wp.pl>
2018-03-26 15:35:03 +0200

    Change .js

--- components/app.js
+++ components/app.js
@@ -26,13 +26,10 @@ var contactForm = {
 
 var App = React.createClass({
     render: function() {
-        return ( <
-            div className = 'app' >
-            <
-            ContactForm contact = { contactForm }
-            /> <
-            Contacts items = { contacts }
-            /></div >
-        );
+        return (
+            <div className = 'app'>
+                <ContactForm contact = { contactForm }/>
+                <Contacts items = { contacts }/>
+            </div >);
     }
 });
--- components/contact.js
+++ components/contact.js
@@ -4,24 +4,18 @@ var Contact = React.createClass({
     },
 
     render: function() {
-        return ( <
-            div className = 'contactItem' >
-            <
-            img className = 'contactImage'
-            src = { 'link-do-obrazka.png' }
-            /> <
-            p className = 'contactLabel' >
-            Imię: {
-                this.props.item.firstName
-            } <
-            /p> <
-            p className = 'contactLabel' >
-            Nazwisko: { this.props.item.lastName } <
-            /p>     <
-            a className = 'contactEmail'
-            href = { 'mailto:' + this.props.item.email } > { this.props.item.email } <
-            /a> < /
-            div >
+        return (
+            <div className = 'contactItem' >
+                < img className ={'contactImage'} src = { 'link-do-obrazka.png' }/> 
+                <p className = {'contactLabel'} >
+                    Imię: {this.props.item.firstName}
+                </p>
+                 <p className = {'contactLabel'} >
+                    Nazwisko: { this.props.item.lastName }
+                </p>
+                <a className = {'contactEmail'} href = {'mailto:' + this.props.item.email } > { this.props.item.email }
+                </a>
+            </div >
         );
     }
 });
